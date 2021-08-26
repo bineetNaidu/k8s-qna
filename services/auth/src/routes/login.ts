bb972576a32379f581fe88b6cbbd311ca61b02a0
bineetNaidu <bineet.s.naidu@gmail.com>
2021-08-26 10:22:49 +0530

    🚀 add login logic

--- services/auth/src/routes/login.ts
+++ services/auth/src/routes/login.ts
@@ -1,9 +1,38 @@
 import { Router } from 'express';
+import jwt from 'jsonwebtoken';
+import { User } from '../models/User';
+import argon from 'argon2';
 
 const r = Router();
 
-r.get('/login', (req, res) => {
-  res.send('Auth service');
+r.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(401).json({
+      message: 'Username Does not Exist!.',
+    });
+  }
+
+  const validPassword = await argon.verify(user.password, password);
+
+  if (!validPassword) {
+    return res.status(401).json({
+      message: 'Password is incorrect!',
+    });
+  }
+  const token = jwt.sign(
+    {
+      id: user._id,
+      username: user.username,
+    },
+    'SECRET'
+  );
+
+  return res.json({
+    token,
+    data: user,
+  });
 });
 
 export { r as loginRouter };
